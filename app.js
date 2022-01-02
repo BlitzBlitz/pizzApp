6afe27c22f2412a2b4caf1a0cf0b467cd58aeebd
Klement Binaj <klementbinaj@gmail.com>
2022-01-02 09:23:55 +0300

    editting ingredients algorithm tested on vecotrs

--- app.js
+++ app.js
@@ -43,3 +43,25 @@ sequelize
   .catch((err) => {
     console.log("Could not connect to DB" + err);
   });
+
+//Tests
+
+let oldIngredients = ["cheese", "tomato", "sauce"];
+let newIngredients = ["cheese", "water", "tomato"];
+
+oldIngredients.forEach((ingredient) => {
+  let newIngIndex = newIngredients.indexOf(ingredient);
+  if (newIngIndex == -1) {
+    oldIngredients.splice(newIngIndex, 1);
+  }
+});
+
+newIngredients.forEach((ingredient) => {
+  let oldIngIndex = oldIngredients.indexOf(ingredient);
+  if (oldIngIndex == -1) {
+    oldIngredients.push(ingredient);
+  }
+});
+
+console.log(oldIngredients);
+console.log(newIngredients);
